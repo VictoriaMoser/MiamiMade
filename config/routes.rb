f8f4b1b9b36af6e729e14d980446a9e645bafd45
Victoria Moser <moservictoria02@gmail.com>
2016-07-28 17:48:17 -0400

    added routes for secret and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'page/index'
+  root 'page#index'
 
-  get 'page/secret'
+  get '/secret' => 'page#secret'
+  get '/register' => 'users#new'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
