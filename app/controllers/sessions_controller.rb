3e1f32acd13e9b3a5ff8955a841b4b8b0021312d
Victoria Moser <moservictoria02@gmail.com>
2016-07-29 16:08:01 -0400

    added a login path to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end 
   end
 
   def destroy
