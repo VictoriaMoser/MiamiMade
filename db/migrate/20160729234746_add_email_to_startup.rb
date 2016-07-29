45d3ea4624ed7fb3ddca651a4849d782895fde6d
Victoria Moser <moservictoria02@gmail.com>
2016-07-29 19:51:41 -0400

    added email column to startup table

--- db/migrate/20160729234746_add_email_to_startup.rb
+++ db/migrate/20160729234746_add_email_to_startup.rb
@@ -1,4 +1,5 @@
 class AddEmailToStartup < ActiveRecord::Migration[5.0]
   def change
+    add_column :startups, :email, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729234458) do
+ActiveRecord::Schema.define(version: 20160729234746) do
 
   create_table "investors", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160729234458) do
     t.boolean  "approval"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "email"
   end
 
   create_table "users", force: :cascade do |t|
