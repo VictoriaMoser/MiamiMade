5b0997ac3ff1a9f5a663b855fcc7e52f4ddf6b71
Victoria Moser <moservictoria02@gmail.com>
2016-07-30 19:49:22 -0400

    fixed startups route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   #
   # get 'investor/destroy'
 
-  get 'startup#index'
 
   # get 'startup/new'
   #
@@ -26,7 +25,6 @@ Rails.application.routes.draw do
   get '/admin_panel' => 'users#admin_panel'
   get '/secret' => 'page#secret'
   get '/register' => 'users#new'
-
   resources :users
 
   #routes for login form/login in/logging out
@@ -35,5 +33,9 @@ Rails.application.routes.draw do
   post '/login'=> 'sessions#create'
 
   get '/logout' => 'sessions#destroy'
+
+  #routes for startups
+    resources :startups
+    get '/startup/index' => 'startups#index'
 
 end
