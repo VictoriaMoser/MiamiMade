90de256a6d4544212669fd109447bbd45dfb89d9
Victoria Moser <moservictoria02@gmail.com>
2016-08-01 15:43:53 -0400

    Undo changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
-  validates :password, length: {minimum: 6}
 end
