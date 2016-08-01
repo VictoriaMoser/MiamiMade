21808f598d474204d6d2a76a7933f3285b0b9a95
Victoria Moser <moservictoria02@gmail.com>
2016-08-01 17:14:59 -0400

    wrote register test

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class LoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    u = User.new(name: "Victoria", email:"v@gmail.com", password:"p")
+    u.save!
+  end
+  test "successful login" do
+     # visit the homepage
+     visit root_path
+     #click the login link
+     click_link "Login"
+     #fill in email
+     fill_in "Email", with: "v@gmail.com"
+     #fill in password
+     fill_in "Password", with: "p"
+     #click on login button
+     click_button "Login"
+     #expect to see signed in as name
+     assert page.has_content?("Signed in as V"), "Failed to login"
+  end
 end
