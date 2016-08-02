3782758c8bcc71944b968017f180256f6b7ad616
Victoria Moser <moservictoria02@gmail.com>
2016-08-02 16:52:07 -0400

    updated startups_test, added lauchy to gemfile

--- test/integration/startups_index_test.rb
+++ test/integration/startups_index_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StartupsIndexTest < ActionDispatch::IntegrationTest
+  setup do
+    s = Startup.new(name:"Victoria's Secret", email:"vs@gmail.com", website:"www.victoriasecret.com", address:"VS Main St", founded_date:"2016-03-02", approval: true)
+    s.save!
+
+  end
+
   test "Startups index page" do
     #visit the home page
     visit root_path
@@ -11,6 +17,8 @@ class StartupsIndexTest < ActionDispatch::IntegrationTest
     #click on learn more about an specific startup
     click_link "Learn More"
     #expect to read more information about each startup
-    assert page.has_content?("More information about the startup")
+    ["Victoria's Secret", "vs@gmail.com", "www.victoriasecret.com","2016-03-02"].each do |content|
+      assert page.has_content?(content)
+    end
   end
 end
