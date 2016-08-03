dc37f7094cf81a1fda06dbc77d6c5abe777fd522
Victoria Moser <moservictoria02@gmail.com>
2016-08-03 12:26:32 -0400

    added default - false to the email confirmation column

--- db/migrate/20160803160840_add_email_confirmation_to_user.rb
+++ db/migrate/20160803160840_add_email_confirmation_to_user.rb
@@ -1,6 +1,6 @@
 class AddEmailConfirmationToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :email_confirmation, :boolean
+    add_column :users, :email_confirmation, :boolean, :default => false 
     add_column :users, :token_confirmation, :string
   end
 end
