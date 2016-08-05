262fbd09cd0647921fe9e8efcb60c9ab0f7fb65c
Victoria Moser <moservictoria02@gmail.com>
2016-08-04 20:08:48 -0400

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,23 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email].downcase)
+    @user = User.find_by(email: params[:email])
+    # debugger;
     if @user && @user.authenticate(params[:password])
-      if user.email_confirmed
-        sign_in user
-        redirect_back_or user
-
+      logger.debug("*****************")
+      if @user.email_confirmation
+        redirect_to @user
       else
-      flash.now[:error]
-      'Please activate your account by following the
-      instructions in the account confirmation email you received to proceed'
-      render 'new'
+        logger.debug("*****************")
+        flash.now[:error] = 'Please check your email to activate your account'
+        render 'new'
       end
     else
-    flash.now[:error] = 'Invalid email/password combination'
-    render 'new'
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
     end
-  end 
+  end
 
   def destroy
     session[:user_id] = nil
