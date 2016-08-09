25a01d72ad6466e636491db18314572e9c6bd10d
Victoria Moser <moservictoria02@gmail.com>
2016-08-09 12:30:51 -0400

    undo admin email confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,9 @@ class UserMailer < ApplicationMailer
     mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
   end
 
-  def admin_confirmation(user)
-    @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Approve New Member")
-  end
+  # def admin_confirmation(user)
+  #   @user = user
+  #   mail(:to => "#{user.name} <#{user.email}>", :subject => "Approve New Member")
+  # end
 
 end
